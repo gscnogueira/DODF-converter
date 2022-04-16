41996037f6d3e14ba4a0b1e74bd9500b34535eb5
Gabriel Nogueira <gab.nog94@gmail.com>
2022-04-15 23:19:25 -0300

    Add teqdm support

--- dodf-converter.py
+++ dodf-converter.py
@@ -1,6 +1,7 @@
 #!/bin/python
 import sys
 import pandas as pd
+from tqdm import tqdm 
 from utils.converter import gen_json
 from utils.template import get_template
 
@@ -9,8 +10,8 @@ path = sys.argv[1]
 df = pd.read_csv(path)
 tipos_ato = df.dropna().tipo_rel.unique()
 
-print(tipos_ato)
-for tipo_ato in tipos_ato:
+print(f"Converting data from '{path}'...")
+for tipo_ato in tqdm(tipos_ato):
 
     df_ato = df[df.tipo_rel ==  tipo_ato]
     json_content = gen_json(df_ato)
@@ -22,5 +23,8 @@ for tipo_ato in tipos_ato:
     html_file = open(tipo_ato + '.html', 'w')
     html_file.write(html_content)
     html_file.close()
+
+print(f"Done!")
+
 
     
